0e18651591c849802b660ae613e8d01aed21c4f6
Emmett Peters <ejp8611@gmail.com>
2017-10-19 16:50:50 -0500

    hilarious loops made with my first java conditionals and loops. you gotta make yourself laugh, you know?"

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,12 +2,39 @@ import java.util.Scanner;
 
 public class HelloWorld {
     public static void main(String[] args){
-        Scanner suckAnut = new Scanner(System.in);
-        System.out.println("Enter a number you want to multiply 5 by: ");
-        int userInput = Integer.parseInt(suckAnut.nextLine());
+//        Scanner suckAnut = new Scanner(System.in);
+//        System.out.println("Enter a number you want to multiply 5 by: ");
+//        int userInput = Integer.parseInt(suckAnut.nextLine());
+//
+//        String result = "5 times " + userInput + " equals " + (int)userInput * 5;
+//        System.out.println(result);
 
-        String result = "5 times " + userInput + " equals " + (int)userInput * 5;
-        System.out.println(result);
+        int startNum = 0;
+        Scanner suckAnut = new Scanner(System.in);
+        int x = 1;
+
+        while(1 > 0) {
+            if (x == 1) {
+                while (startNum <= 69) {
+                    System.out.println("what number would you like to add?");
+                    int userInput = Integer.parseInt(suckAnut.nextLine());
+                    startNum += userInput;
+                    System.out.println("The number is now " + startNum);
+                }
+                x = 0;
+                System.out.println("Your number is over 69 so were going to be subtracting from it now");
+            }
+
+            if(x == 0) {
+                while (startNum > -69) {
+                    System.out.println("What number would you like to subtract from your number?");
+                    int userInput = Integer.parseInt(suckAnut.nextLine());
+                    startNum -= userInput;
+                    System.out.println("The number is now " + startNum);
+                }
+                System.out.println("Your number is lower than -69 so were going to be adding to it now");
+                x = 1;
+            }
+        }
     }
 }
-
