61dc4722c1a9d0b16e716c7a72f097c3738a7371
Emmett Peters <ejp8611@gmail.com>
2017-10-19 14:53:08 -0500

    second exercis finished

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,10 +1,15 @@
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args){
         int input1 = 23;
         double input2 = 24.43;
         int input3 = (int) input2;
-        String myFavNum = "69";
+        double myFavNum = 1993.26;
 
-        System.out.println(input3 * input1);
+//        char favNum = (char) myFavNum;
+
+        System.out.println(++myFavNum);
     }
 }
+
