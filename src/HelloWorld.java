7f2f74f61b82a5ae6e10e148f8d2548a7ad13354
Emmett Peters <ejp8611@gmail.com>
2017-10-19 14:23:52 -0500

    first exercise complete on java.codeup.com

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        int input1 = 23;
+        double input2 = 24.43;
+        int input3 = (int) input2;
+        String myFavNum = "69";
+
+        System.out.println(input3 * input1);
+    }
+}
