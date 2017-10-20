f0371a2524d00f903ef99e2445eb3ccee94d0779
Emmett Peters <ejp8611@gmail.com>
2017-10-20 18:15:22 -0500

    uhh... more work done...

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -9,32 +9,41 @@ public class HelloWorld {
 //        String result = "5 times " + userInput + " equals " + (int)userInput * 5;
 //        System.out.println(result);
 
-        int startNum = 0;
-        Scanner suckAnut = new Scanner(System.in);
-        int x = 1;
+//        int startNum = 0;
+//        Scanner suckAnut = new Scanner(System.in);
+//        int x = 1;
+//
+//        while(1 > 0) {
+//            if (x == 1) {
+//                while (startNum <= 69) {
+//                    System.out.println("what number would you like to add?");
+//                    int userInput = Integer.parseInt(suckAnut.nextLine());
+//                    startNum += userInput;
+//                    System.out.println("The number is now " + startNum);
+//                }
+//                x = 0;
+//                System.out.println("Your number is over 69 so were going to be subtracting from it now");
+//            }
+//            if(x == 0) {
+//                while (startNum > -69) {
+//                    System.out.println("What number would you like to subtract from your number?");
+//                    int userInput = Integer.parseInt(suckAnut.nextLine());
+//                    startNum -= userInput;
+//                    System.out.println("The number is now " + startNum);
+//                }
+//                System.out.println("Your number is lower than -69 so were going to be adding to it now");
+//                x = 1;
+//            }
+//        }
 
-        while(1 > 0) {
-            if (x == 1) {
-                while (startNum <= 69) {
-                    System.out.println("what number would you like to add?");
-                    int userInput = Integer.parseInt(suckAnut.nextLine());
-                    startNum += userInput;
-                    System.out.println("The number is now " + startNum);
-                }
-                x = 0;
-                System.out.println("Your number is over 69 so were going to be subtracting from it now");
-            }
+//        System.out.println("What number do you need the multiples to?");
+//        Scanner question = new Scanner(System.in);
+//        int userInput = Integer.parseInt(question.nextLine());
+//        System.out.println("number  |  squared  | cuded");
+//        for(int x = 1; x <= userInput; ++x){
+//            System.out.println(x + "     | " + x*x + "    | " + x*x*x );
+//        }
 
-            if(x == 0) {
-                while (startNum > -69) {
-                    System.out.println("What number would you like to subtract from your number?");
-                    int userInput = Integer.parseInt(suckAnut.nextLine());
-                    startNum -= userInput;
-                    System.out.println("The number is now " + startNum);
-                }
-                System.out.println("Your number is lower than -69 so were going to be adding to it now");
-                x = 1;
-            }
-        }
+        
     }
 }
